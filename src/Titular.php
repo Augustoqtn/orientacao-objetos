32b3bc611f6863f35d9f63bfdf6faec86e3f506a
Augusto <augustinhobrito542@gmail.com>
2022-01-26 17:42:53 -0300

    apaga arquivos com letras minisculas

--- src/Titular.php
+++ src/Titular.php
@@ -1,26 +1,25 @@
 <?php
 class Titular
 {
-    
-private $cpf;
-private $nome;
+    private $cpf;
+    private $nome;
 
     public function __construct(string $cpf, string $nome)
     {
         $this->cpf = $cpf;
-        $this->validaNomeTitular($nome);
         $this->nome = $nome;
     }
 
-    public function recuperaCpf():string
+    public function recuperaCpf(): string
     {
         return $this->cpf;
     }
 
-    public function recuperaNome():string
+    public function recuperaNome(): string
     {
         return $this->nome;
     }
+
 
     private function validaNomeTitular(string $nomeTitular)
     {
